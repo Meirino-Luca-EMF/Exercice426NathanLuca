52e234e0df33966fbef84b1baac32d1e49b97480
Meirino.Luca.emf <Luca.Meirino@studentfr.ch>
2026-03-17 11:20:48 +0100

    save

--- exercice/app/threads/NombreB.java
+++ exercice/app/threads/NombreB.java
@@ -1,10 +1,8 @@
 package exercice.app.threads;
 
-import java.util.ArrayList;
-
 public class NombreB extends Thread {
     
-    private final int nombre;
+    private int nombre;
     private final int min;
     private final int max;
     private volatile boolean travailRealise;
@@ -19,12 +17,40 @@ public class NombreB extends Thread {
 
     @Override
     public void run() {
-        System.out.println("Le deuxième nombre aléatoire a démarré !");
+        System.out.println("Le deuxième générateur de nombre aléatoire a démarré !");
 
-        
+        nombre = min + (int)(Math.random() * ((max - min) + 1));
+        dormir(1); // pour simuler que ça prend du temps...
 
         travailRealise = true;
 
         System.out.println("Le deuxième générateur de nombre aléatoire a terminé !");
     }
+
+    public void dormir(int ms) {
+        try {
+            sleep(ms);
+        } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    public int getNombres() {
+
+        if (!travailRealise) {
+            // forcer à attendre que le thread ait fini :-)
+            try {
+                System.out.println(" ==> on va attendre que générateur de nombres aléatoires ait terminé !");
+                this.join();
+                System.out.println(" ==> le générateur de nombres aléatoires a fini !");
+            } catch (InterruptedException ex) {
+                System.out.println(ex.getMessage());
+                ex.printStackTrace();
+            }
+        }
+
+        return nombre;
+    }
 }
+
