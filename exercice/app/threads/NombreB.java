27b50059744162e770d198a26a6fe184f5c99010
Meirino.Luca.emf <Luca.Meirino@studentfr.ch>
2026-03-17 11:12:32 +0100

    save

--- exercice/app/threads/NombreB.java
+++ exercice/app/threads/NombreB.java
@@ -1,5 +1,30 @@
 package exercice.app.threads;
 
-public class NombreB {
+import java.util.ArrayList;
+
+public class NombreB extends Thread {
     
+    private final int nombre;
+    private final int min;
+    private final int max;
+    private volatile boolean travailRealise;
+
+    public NombreB(int min, int max) {
+        this.travailRealise = false;
+        this.nombre = 0;
+        this.min = min;
+        this.max = max;
+        setName("Je suis le générateur pour le deuxième nombre");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Le deuxième nombre aléatoire a démarré !");
+
+        
+
+        travailRealise = true;
+
+        System.out.println("Le deuxième générateur de nombre aléatoire a terminé !");
+    }
 }
