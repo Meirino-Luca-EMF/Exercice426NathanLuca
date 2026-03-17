af682967604e25f195ab9f8853109b2c23f41fc6
Nathan <Nathan.Ruffieux03@studentfr.ch>
2026-03-17 11:32:34 +0100

    road to controller

--- exercice/app/app.java
+++ exercice/app/app.java
@@ -1,37 +1,10 @@
 package exercice.app;
 
-import java.util.ArrayList;
-
-import exercice.threads.NombreThread;
+import exercice.ctrl.Ctrl;
 
 public class app {
     public static void main(String[] args) {
-        
-        try {
-            // Créer 3 générateurs de nombres aléatoires (entre 1 et 7)
-            NombreThread a1 = new NombreThread(1, 7);
-            NombreThread a2 = new NombreThread(1, 7);
-            NombreThread a3 = new NombreThread(1, 7);
-            // Démarrer les générateurs
-            a1.start();
-            a2.start();
-            a3.start();
-
-            // Attendre qu'ils aient fini
-            a1.join();
-            a2.join();
-            a3.join();
-
-            ArrayList<Integer> tousLesNombres = new ArrayList<>();
-            tousLesNombres.add(a1.getNombre());
-            tousLesNombres.add(a2.getNombre());
-            tousLesNombres.add(a3.getNombre());
-
-            System.out.println(tousLesNombres);
-
-        } catch (InterruptedException ex) {
-            System.out.println(ex.getMessage());
-            ex.printStackTrace();
-        }
+        Ctrl ctrl = new Ctrl();
+        ctrl.start();
     }
 }
