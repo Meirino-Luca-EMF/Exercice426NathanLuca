2c414e43608aaf92692243a4260f0b693ec46ae3
Nathan <Nathan.Ruffieux03@studentfr.ch>
2026-03-17 11:27:33 +0100

    un petit peu de style

--- exercice/app/app.java
+++ exercice/app/app.java
@@ -1,5 +1,7 @@
 package exercice.app;
 
+import java.util.ArrayList;
+
 import exercice.threads.NombreThread;
 
 public class app {
@@ -8,16 +10,24 @@ public class app {
         try {
             // Créer 3 générateurs de nombres aléatoires (entre 1 et 7)
             NombreThread a1 = new NombreThread(1, 7);
-
+            NombreThread a2 = new NombreThread(1, 7);
+            NombreThread a3 = new NombreThread(1, 7);
             // Démarrer les générateurs
             a1.start();
+            a2.start();
+            a3.start();
 
             // Attendre qu'ils aient fini
             a1.join();
+            a2.join();
+            a3.join();
 
-            int test = a1.getNombre();
+            ArrayList<Integer> tousLesNombres = new ArrayList<>();
+            tousLesNombres.add(a1.getNombre());
+            tousLesNombres.add(a2.getNombre());
+            tousLesNombres.add(a3.getNombre());
 
-            System.out.println(test);
+            System.out.println(tousLesNombres);
 
         } catch (InterruptedException ex) {
             System.out.println(ex.getMessage());
